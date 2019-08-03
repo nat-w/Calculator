9b7cf8bd8c71ba1c6df23a58485a76c203a47b0c
pshivali <p.shivali@hotmail.com>
2019-08-02 23:45:51 -0400

    Update power.java

--- src/power.java
+++ src/power.java
@@ -1,43 +1,27 @@
-import java.util.Scanner;
-
 public class power {
-	public static void main(String[] args){
-	Scanner kb = new Scanner(System.in);
-	{
-		int base=10;
-		int exponent=1;
-		kb = new Scanner(System.in);
-		
-		System.out.print(" Please Enter the Exponent Value : ");
-		exponent = kb.nextInt();	
-		double result = computepower(base, exponent);
-		System.out.println(base+ " to the power of "+ exponent +" = "+ result +".");
-		}
-	}
-	
-	
-	private static double computepower(int base, int exponent) {
-		 double result = 1;
-		    if(exponent==0)// base case
-		       return 1;
-		    
-		    if(exponent>0){
-		         for(int i = 0;i<exponent;i++)
-		         { 
-		              result *= base;
-		         }
-		    }
-		    else
-		    {
-		      for(int i = exponent;i<0;i++)
-		         {  
-		              result *= base;
-		         }
-		           result = 1/result;
-		    }
-		    return result;
-		}
+    
+     static double calculate(double x) {
+         double result = 1;
+         double base=10;
+         double exponent=(int)x;
+	     
+          if(exponent==0)// base case
+          	return 1;
+            
+          if(exponent>0){
+             for(int i = 0;i<exponent;i++)
+                 {
+                      result *= base;
+                 }
+            }
+            else
+            {
+              for(double i = exponent;i<0;i++)
+                 {
+                      result *= base;
+                 }
+                   result = 1/result;
+            }
+            return result;
+        }
 }
-
-
-
